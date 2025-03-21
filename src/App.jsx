4ec1204cc80540404f28b58e7b7956f86325b4f6
Rojda Polat <rojdapolat@outlook.de>
2025-03-21 17:57:02 +0100

    Routes done

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,36 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HomepagePage from "./pages/HomepagePage";
+import DogsPage from "./pages/DogsPage";
+import CatsPage from "./pages/CatsPage";
+import SmallPetsPage from "./pages/SmallPetsPage";
+import BirdsPage from "./pages/BirdsPage";
+import FishPage from "./pages/BirdsPage";
+import DetailsPage from ".pages/DetailsPage";
+import AboutPage from ".pages/AboutPage";
+import AdminsPage from ".pages/AdminsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Pet Shop</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/" element={<HomepagePage />} />
+        <Route path="/pets/dogs" element={<DogsPage />} />
+        <Route path="/pets/cats" element={<CatsPage />} />
+        <Route path="/pets/smallpets" element={<SmallPetsPage />} />
+        <Route path="/pets/birds" element={<BirdsPage />} />
+        <Route path="/pets/fish" element={<FishPage />} />
+        <Route path="/pets/:productId" element={<DetailsPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/admin" element={<AdminsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
   )
 }
