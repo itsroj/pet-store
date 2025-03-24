f884eb6b545800b43e8ef9395485d1db8449ae68
Rojda Polat <rojdapolat@outlook.de>
2025-03-24 16:15:08 +0100

    create homepage and started dogspage with listing the products

--- src/pages/DogsPage.jsx
+++ src/pages/DogsPage.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 
+
 const DogsPage = () => {
+  const [dogProducts, setDogProducts] = useState([]);
+   //here we need get all of the projects from our server
+   useEffect(() => {
+    axios
+      .get("http://localhost:5005/products")
+      .then((res) => {
+        console.log(res.data);
+        const filteredData = res.data.filter((oneProduct) => {
+          if (oneProduct.animal_type === "dogs") {
+            return true;
+          }
+        })
+        setDogProducts(filteredData)
+      })
+      .catch((err) => console.log(err));
+    }, []);
+
   return (
     <div>
-      There is the dogs products
+        There is the dogs products
+      <button>snacks</button>
+      <div>
+      {dogProducts.map((oneProduct) => {
+        return <div key={oneProduct.id}>
+          <h3>{oneProduct.name}</h3>
+        </div>
+      })}
+      </div>
     </div>
   )
 }
--- src/pages/HomepagePage.jsx
+++ src/pages/HomepagePage.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ShopByCategory from '../components/ShopByCategory'
 import Bestsellers from '../components/Bestsellers'
+import axios from 'axios';
 
 const HomepagePage = () => {
+
   return (
     <div>
       <ShopByCategory />
