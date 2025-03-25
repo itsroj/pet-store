3540c8d0c5758502569f55e61fc0333730afe8a9
Rojda Polat <rojdapolat@outlook.de>
2025-03-25 12:45:51 +0100

    added delete btn and some new components to edit and add

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const AddItemForm = () => {
+const AddProduct = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const AddItemForm = () => {
   )
 }
 
-export default AddItemForm
+export default AddProduct
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import { Link } from "react-router-dom";
+import "../components/EditProduct";
+import "../components/AddProduct";
 
 const AdminPage = () => {
 
   const [allProducts, setAllProducts] = useState([]);
   
-   //here we need get all of the projects from our server
+   //here we need get all of the products from our server
    useEffect(() => {
     axios
       .get("http://localhost:5005/products")
@@ -17,6 +19,22 @@ const AdminPage = () => {
    }).catch((err) => console.log(err));
     }, []);
 
+    function handleDelete(productId) { 
+      //first remove from the frontend
+      const filteredProducts = allProducts.filter(
+        (oneProduct) => productId !== oneProduct.id
+      );
+      console.log("delete clicked", productId, filteredProducts);
+      setAllProducts(filteredProducts);
+      
+      //second step is to send a request to the json server to delete one product
+      axios
+        .delete(`http://localhost:5005/products/${productId}`)
+        .then((response) => {
+          console.log("product deleted... nice work", response.data);
+        })
+        .catch((error) => console.log(error));
+    }
 
   return (
     <div>
@@ -24,21 +42,28 @@ const AdminPage = () => {
       <div className="productContainer">
         {allProducts.map((oneProduct) => {
           return (
-          <div key={oneProduct.id} className="productCard">
-            <Link to ={`/pets/${oneProduct.id}`}>
-              <img src={oneProduct.image} alt="product image"/>
-            </Link>
-            <h4>{oneProduct.name}</h4>
-            <p>Category: {oneProduct.category}</p>
-            <p>Animal Type: {oneProduct.animal_type}</p>
-            <p>Price: {oneProduct.price}€</p>
-            <p>Stock: {oneProduct.stock}</p>
-            <p>Description: {oneProduct.description}</p>
-            <button>Edit</button>
-            <button>Delete</button>
-        </div>)
-      })}
-    </div>
+            <div key={oneProduct.id} className="productCard">
+              <Link to={`/pets/${oneProduct.id}`}>
+                <img src={oneProduct.image} alt="product image"/>
+              </Link>
+              <h4>{oneProduct.name}</h4>
+              <p>Category: {oneProduct.category}</p>
+              <p>Animal Type: {oneProduct.animal_type}</p>
+              <p>Price: {oneProduct.price}€</p>
+              <p>Stock: {oneProduct.stock}</p>
+              <p>Description: {oneProduct.description}</p>
+              <button>Edit</button>
+              <button
+                onClick={() => {
+                  handleDelete(oneProduct.id);
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   )
 }
